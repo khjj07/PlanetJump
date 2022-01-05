77c391c81578daa8c727d0a39336b6595ba97d26
khjj07 <khjj07@gmail.com>
2022-01-05 22:07:48 +0900

    meteor init

---
+++ assets/meteor/spawner/meteor_spawner.go
@@ -0,0 +1,33 @@
+components {
+  id: "meteor_spawner"
+  component: "/assets/meteor/spawner/meteor_spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "meteor"
+  type: "factory"
+  data: "prototype: \"/assets/meteor/meteor.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
