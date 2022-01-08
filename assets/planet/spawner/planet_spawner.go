7db6de5338ab57ba40fafb99f1ccf7e0d48f6b73
khjj07 <khjj07@gmail.com>
2022-01-08 20:11:21 +0900

    행성 생성

---
+++ assets/planet/spawner/planet_spawner.go
@@ -0,0 +1,33 @@
+components {
+  id: "planet_spawner"
+  component: "/assets/planet/spawner/planet_spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "planet"
+  type: "collectionfactory"
+  data: "prototype: \"/assets/planet/planet.collection\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
