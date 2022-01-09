70f3ec97b891449ebad00be4404686d81f7bcbe0
ihaha424 <70957529+ihaha424@users.noreply.github.com>
2022-01-09 18:47:41 +0900

    쓰래기

--- assets/meteor/spawner/meteor_spawner.go
+++ assets/meteor/spawner/meteor_spawner.go
@@ -16,7 +16,25 @@ components {
 embedded_components {
   id: "meteor"
   type: "factory"
-  data: "prototype: \"/assets/meteor/meteor.go\"\n"
+  data: "prototype: \"/assets/camera/meteor.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "big_meteor"
+  type: "factory"
+  data: "prototype: \"/assets/meteor/big_meteor.go\"\n"
   "load_dynamically: false\n"
   ""
   position {
