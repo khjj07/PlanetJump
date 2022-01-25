d68feacc39bcaf11e02944b4161d3131b5eec741
khjj07 <khjj07@gmail.com>
2022-01-26 08:31:29 +0900

    버그 수정 및 파편 개선

--- assets/satellite/satellite.go
+++ assets/satellite/satellite.go
@@ -28,6 +28,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "satellite_explosion"
+  component: "/assets/effect/satellite_explosion.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
