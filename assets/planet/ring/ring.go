662b1e2e50406423b34ff98f515d28fed89c5783
DESKTOP-63HRE8U\KOCCA18 <khjj07@gmail.com>
2022-09-13 17:04:23 +0900

    ring effect1

--- assets/planet/ring/ring.go
+++ assets/planet/ring/ring.go
@@ -28,6 +28,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "twinkle_spread"
+  component: "/assets/planet/ring/twinkle_spread.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
